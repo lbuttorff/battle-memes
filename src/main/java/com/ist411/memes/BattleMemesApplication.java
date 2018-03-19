22459b91bc971f3220a7111238cfe0767b792adc
Lowell Buttorff <lbuttorff@gmail.com>
2018-03-19 14:42:19 -0400

    Initial commit of Spring project.

---
+++ src/main/java/com/ist411/memes/BattleMemesApplication.java
@@ -0,0 +1,12 @@
+package com.ist411.memes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleMemesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleMemesApplication.class, args);
+	}
+}
