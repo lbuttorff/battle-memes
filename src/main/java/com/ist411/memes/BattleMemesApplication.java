089acac1e8760ccf51ffcaffac14a9757702bb1d
Lowell Buttorff <lbuttorff@gmail.com>
2018-04-09 19:04:52 -0400

    It still doesn't work, pls fix eventually.

--- src/main/java/com/ist411/memes/BattleMemesApplication.java
+++ src/main/java/com/ist411/memes/BattleMemesApplication.java
@@ -2,9 +2,11 @@ package com.ist411.memes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ist411.memes.services"})
 public class BattleMemesApplication {
 
 	public static void main(String[] args) {
